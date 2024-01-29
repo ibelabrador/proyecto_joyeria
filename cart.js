58771ec361f67f8e1ae3fc0a04928025f49c16dd
ibelabrador <ibelabrador@gmail.com>
2024-01-28 21:08:06 -0500

    Creación pag Cart y lista de productos

---
+++ cart.js
@@ -0,0 +1,139 @@
+const listaDeProductos = [
+{
+  id: "jewel1",
+  nombre: "Radiance Necklace",
+  codigo: 70000,
+  precioUnitario:168.76,
+  tipoDeAccesorio: "collar",
+  imagen:[""] ,
+  descripcion: "",
+  cantidad: "",
+},
+
+{
+  id: "jewel2",
+  nombre: "Exquisite Earrings",
+  codigo: "70001",
+  precioUnitario: 125.28,
+  tipoDeAccesorio: "aretes",
+  imagen:[],
+  descripcion:"Aretes con diamante",
+  stockPorColor: {
+    plata: 10,
+    dorado: 5,
+  }
+},
+
+{
+  id: "jewel3",
+  nombre: "Glamour Necklace",
+  codigo: "70002",
+  precioUnitario: 620.73,
+  tipoDeAccesorio: "collar",
+  imagen: [],
+  descripcion: "Collar con perlas",
+  stockPorColor: {
+    plata: 3,
+    dorado: 5,
+  },
+},
+
+{
+  id: "jewel4",
+  nombre: "Delights Earrings",
+  codigo: "70003",
+  precioUnitario: 327.71,
+  tipoDeAccesorio: "aretes",
+  imagen: "",
+  descripcion: "Aretes de oro",
+  stockPorColor: {
+    plata: 10,
+    dorado: 2,
+  },
+},
+
+{
+  id: "jewel5",
+  nombre: "Luxurious Lustre",
+  codigo: "70004",
+  precioUnitario: 125.28,
+  tipoDeAccesorio: "aretes",
+  imagen: "",
+  descripcion: "Aretes con amatista",
+  stockPorColor: {
+    plata: 3,
+    dorado: 2,
+  },
+},
+
+{
+  id: "jewel6",
+  nombre: "Blissful Baubles",
+  codigo: "70005",
+  precioUnitario: 125.28,
+  tipoDeAccesorio: "anillo",
+  imagen: "",
+  descripcion: "Anillo de diamante",
+  stockPorColor: {
+    plata: 8,
+    dorado: 15,
+  },
+},
+
+{
+  id: "jewel7",
+  nombre: "Radiant Reflections",
+  codigo: "70006",
+  precioUnitario: 327.71,
+  tipoDeAccesorio: "collar",
+  imagen: "",
+  descripcion: "Collar doble",
+  stockPorColor: {
+    plata: 20,
+    dorado: 19,
+  },
+},
+
+{
+  id: "jewel8",
+  nombre: "Timeless Treasures",
+  codigo: "70007",
+  precioUnitario: 125.28,
+  tipoDeAccesorio: "aretes",
+  imagen: "",
+  descripcion: "Aretes largos",
+  stockPorColor: {
+    plata: 12,
+    dorado: 5,
+  },
+},
+
+{
+  id: "jewel9",
+  nombre: "Majestic Mementos",
+  codigo: "70008",
+  precioUnitario: 125.28,
+  tipoDeAccesorio: "collar",
+  imagen: "",
+  descripcion: "Collar largo",
+  stockPorColor: {
+    plata: 9,
+    dorado: 9,
+  },
+},
+
+{
+  id: "jewel10",
+  nombre: "Divine Diamonds",
+  codigo: "70009",
+  precioUnitario: 168.76,
+  tipoDeAccesorio: "brazalete",
+  imagen: "",
+  descripcion: "Brazalete con pedrería",
+  stockPorColor: {
+    plata: 20,
+    dorado: 25,
+  },
+},
+
+]
