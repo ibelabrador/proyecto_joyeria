90ce5e99f3a3fdc6688c86176223b8313e18af32
ibelabrador <ibelabrador@gmail.com>
2024-01-30 10:36:40 -0500

    funciones

--- cart.js
+++ cart.js
@@ -3,7 +3,7 @@ const listaDeProductos = [
   id: "jewel1",
   nombre: "Radiance Necklace",
   codigo: 70000,
-  precioUnitario:168.76,
+  precioUnitario:620.73,
   tipoDeAccesorio: "collar",
   imagen:[""] ,
   descripcion: "",
@@ -112,7 +112,7 @@ const listaDeProductos = [
   id: "jewel9",
   nombre: "Majestic Mementos",
   codigo: "70008",
-  precioUnitario: 125.28,
+  precioUnitario: 620.73,
   tipoDeAccesorio: "collar",
   imagen: "",
   descripcion: "Collar largo",
@@ -137,3 +137,116 @@ const listaDeProductos = [
 },
 
 ]
+
+// function getByType(products, typeProduct){
+//   let filteredProducts = [];
+//   for (let i=0; i<products.length; i++){
+//     if (products[i].tipoDeAccesorio===typeProduct){
+//       filteredProducts.push(products[i]);
+//     }
+//   }
+//   return filteredProducts;
+// }
+
+// var collars = getByType(listaDeProductos, "collar");
+// console.log(collars);
+
+const getByType = (products, typeProduct) => {
+  return products.filter(product => product.tipoDeAccesorio === typeProduct);
+}
+
+console.log(getByType(listaDeProductos, "collar"));
+
+
+
+// function getByName(products, name){
+//   let filteredProducts = [];
+//   for (let i = 0; i < products.length; i++){
+//     if (products[i].nombre.includes(name)){
+//       filteredProducts.push(products[i]);
+//     }
+//   }
+//   return filteredProducts;
+// }
+
+// var productByName = getByName(listaDeProductos, "Necklace");
+// console.log(productByName);
+
+const getByName = (products, name) => {
+  return products.filter(product => product.nombre.includes(name));
+}
+
+console.log(getByName(listaDeProductos, "Necklace"));
+
+
+
+// función para ordenar productos
+
+const sortAsc = (productA, productB) => {
+  if (productA.precioUnitario < productB.precioUnitario){
+    return -1;
+  }
+  else if (productA.precioUnitario > productB.precioUnitario){
+    return 1;
+  }
+  return 0;
+} 
+
+const sortDesc = (productA, productB) => {
+  if (productA.precioUnitario < productB.precioUnitario){
+    return 1;
+  }
+  else if (productA.precioUnitario > productB.precioUnitario){
+    return -1;
+  }
+  return 0;
+} 
+
+const sortProducts = (products, typeSort) => {
+  if (typeSort === "asc"){
+    return products.sort(sortAsc);
+  }
+  else if (typeSort === "desc"){
+    return products.sort(sortDesc);
+  }
+  return products;
+}
+
+console.log(sortProducts(listaDeProductos, "asc"));
+
+
+// Función para sumar productos
+
+var purchasedProducts = [ 
+  {
+    id: "jewel1",
+    quantity: 4,
+  },
+
+  {
+    id: "jewel10",
+    quantity: 2,
+  },
+
+  {
+    id: "jewel5",
+    quantity: 1,
+  }
+
+]
+
+const sumProducts = (products, cartProducts) => {
+  let totalSum = 0;
+  let product;
+  for (let i = 0; i < cartProducts.length; i++){
+    product = products.find(product => product.id === cartProducts[i].id);
+    totalSum += product.precioUnitario * cartProducts[i].quantity;
+  }
+  return totalSum;
+}
+
+console.log(sumProducts(listaDeProductos, purchasedProducts));
+
+
+
+
